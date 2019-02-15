7540442fc5b8b7db5dc1a98634dd418dc12908f8
Memotets <guillermo.bg99@gmail.com>
2019-02-14 19:35:31 -0600

    Primera version de grafo Euleriano

--- test/Grafo.java
+++ test/Grafo.java
@@ -9,5 +9,21 @@
  * @author Hector
  */
 public class Grafo {
+    int grafo[][];
+    int nodos;
+    
+    boolean Euleriano(){
+        int i,j, grado;
+        for(i=0;i<nodos;i++){
+            grado =0;
+            for(j=0;j<nodos;j++){
+                grado+= grafo[i][j];
+            }
+            if(grado%2!=0){
+                return false;
+            }
+        }
+        return true;
+    }
     
 }
